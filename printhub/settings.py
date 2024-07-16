8b93403af438680648a37c975770bcb9f78313d1
Emmanuel John Aguilar <aguilaremman3@gmail.com>
2024-07-16 23:21:02 +0800

    edit settings.py staticfiles

--- printhub/settings.py
+++ printhub/settings.py
@@ -119,12 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# adding project-level static directory
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
 
 #directory to collect all static files for production
 STATIC_ROOT = BASE_DIR / "staticfiles"
