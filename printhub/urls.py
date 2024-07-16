56abcb0a029228f51693467234b0b11a7ed91f81
Emmanuel John Aguilar <aguilaremman3@gmail.com>
2024-07-17 00:55:26 +0800

    update settings.py staticurl

--- printhub/urls.py
+++ printhub/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("printhub_app.urls")),
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
